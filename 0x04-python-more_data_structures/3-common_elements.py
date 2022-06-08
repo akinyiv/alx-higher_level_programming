e875711fc89043cfd1396b2ee3bdf0785540a91b
akinyiv <akinyivee10@gmail.com>
2022-06-08 15:08:26 +0300

    elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    """adds all unique integers in a list (only once for each integer)."""
-    new = set(my_list)
-    result = 0
-    for i in new:
-        result += i
-    return result
+def common_elements(set_1, set_2):
+    return set_1 & set_2
