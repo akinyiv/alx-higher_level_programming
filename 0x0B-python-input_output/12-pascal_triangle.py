0aa24dfe9cb027ca1122baae509f8f7519dd4716
akinyiv <akinyivee10@gmail.com>
2022-07-05 13:00:46 +0300

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,39 +1,22 @@
-#!/usr/bin/env python3
-"""Calculate Pascal's Triangle"""
-import sys
+#!/usr/bin/python3
+"""
+Pascal's Triangle Module
+"""
 
-def get_triangle(num):
-    """return 2D list of triangle"""
-    ret = [[1]]
-    for i in range(1, num + 1):
-        sys.stdout.write('\rrow: '+str(i))
-        row = []
-        for j in range(0, i+1):
-            if j == 0 or j == i:
-                row.append(1) # first or last position
-            else:
-                res = ret[i-1][j-1]+ret[i-1][j]
-                row.append(res)
 
-        ret.append(row)
-    sys.stdout.write('\n')
-    return ret
-
-def main():
-    """Get number of rows to calculate from the user and
-    show a pretty triangle"""
-    try:
-        num = int(input('Number of rows: '))
-    except ValueError:
-        print('Please input an integer')
-        exit(1)
-    triangle = get_triangle(num)
-    length = len(triangle) - 1
-    if length > 100:
-        return print('too large, not displaying')
-    for i in range(0, length):
-        row = triangle[i]
-        print(' '*(length-i)+(' '.join(str(j) for j in row)))
-
-if __name__ == '__main__':
-    main()
+def pascal_triangle(n):
+    """calculate pascal
+    args:
+        n: value
+    return
+        list
+    """
+    my_list = []
+    if n <= 0:
+        return my_list
+    for i in range(n):
+        num = 11**i
+        li = [int(n) for n in str(num)]
+        my_list.append(li)
+    return my_list
+    
